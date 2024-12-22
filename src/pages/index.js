7ddc44f691a7e5c1b8b6b92b329ab0f0669234df
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-12-22 21:56:09 +0500

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,52 @@
-import Head from "next/head";
+import Image from "next/image";
+
+import Layout from "@/components/Layout";
+import AnimatedText from "@/components/AnimatedText";
+
+import profilePic from "../../public/images/profile/developer-pic-1.png";
+import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 
 export default function Home() {
   return (
     <>
-      <main className="">Home</main>
+      <main className="flex items-center text-dark w-full min-h-screen">
+        <Layout className="pt-0">
+          <div className="flex items-center justify-between w-full">
+            <div className="w-1/2">
+              <Image src={profilePic} alt="" className="w-full h-auto" />
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="!text-6xl !text-left"
+              />
+              <p className="my-4 text-base font-medium">
+                As a skilled Web Developer, I am dedicated to turning ideas into
+                innovative web applications. Explore my latest projects and
+                articles, showcasing my expertise in React.js , Next.js and Web
+                Development.
+              </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/Resume.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark"
+                  download={true}
+                >
+                  Resume <LinkArrow className={"w-6 ml-1"} />
+                </Link>
+                <Link
+                  href="mailto:abubakarsaddique786.tsk@gmail.com"
+                  target={"_blank"}
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
     </>
   );
 }
