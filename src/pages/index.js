b858d5f1a72afc4226ea644171e238095e99dfe1
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-12-22 22:03:07 +0500

    third commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main className="flex items-center text-dark w-full min-h-screen">
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
-            <div className="w-1/2">
+            <div className="w-1/2 mr-5">
               <Image src={profilePic} alt="" className="w-full h-auto" />
             </div>
             <div className="w-1/2 flex flex-col items-center self-center">
