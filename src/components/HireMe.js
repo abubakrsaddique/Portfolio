96fad8f35ec92ffe953278f0368a71e4395c161b
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2024-12-23 23:07:31 +0500

    homepage update

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -9,7 +9,7 @@ const HireMe = () => {
         <CircularText className={"fill-dark animate-spin-slow"} />
         <Link
           href="mailto:abubakarsaddique786.tsk@gmail.com"
-          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-24 h-24 rounded-full font-semibold hover:bg-light hover:text-dark"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-[88px] h-[88px] rounded-full font-semibold hover:bg-light hover:text-dark"
         >
           Hire Me
         </Link>
