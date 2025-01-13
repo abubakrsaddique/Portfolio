708992b8bbd587761167ec5bd38c6547dd2dc19c
M Abubakar Saddique <abubakarsaddique786.tsk@gmail.com>
2025-01-13 10:57:05 +0500

    latest commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -33,7 +33,7 @@ const Skills = () => {
         <Skill name="JavaScript" x="20vw" y="6vw" />
         <Skill name="ReactJS" x="0vw" y="12vw" />
         <Skill name="NextJS" x="-20vw" y="-15vw" />
-        <Skill name="BootStrap" x="15vw" y="-12vw" />
+        <Skill name="Typescript" x="15vw" y="-12vw" />
         <Skill name="MoongoDB" x="32vw" y="-5vw" />
         <Skill name="ChakraUi" x="0vw" y="-20vw" />
         <Skill name="Firebase" x="-25vw" y="18vw" />
